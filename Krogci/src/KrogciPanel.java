38a04a54b7ce4d4f9f5d75fba4fdf0caa80ecc7e
KappaCancer <janez.radescek@hotmail.com>
2016-06-08 08:16:16 +0200

    arraylist popravljen za threde

--- Krogci/src/KrogciPanel.java
+++ Krogci/src/KrogciPanel.java
@@ -3,32 +3,37 @@ import java.awt.Graphics;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 import javax.swing.JPanel;
 
 public class KrogciPanel extends JPanel {
 	
-	private ArrayList<Krogec> krogci;
-	private ConcurrentLinkedQueue<Krogec> vrstaKrogcev;
+	//private ArrayList<Krogec> krogci;
+	//private ConcurrentLinkedQueue<Krogec> vrstaKrogcev;
+	
+	private CopyOnWriteArrayList<Krogec> vrstaKrogcev;
 
 	public KrogciPanel() {
 		super();
 		setBackground(Color.black);
-		krogci = new ArrayList<Krogec>();
-		
+		//krogci = new ArrayList<Krogec>();
+		vrstaKrogcev = new CopyOnWriteArrayList<Krogec>();
 	}
 
 	@Override
 	protected void paintComponent(Graphics g){
 		super.paintComponent(g);
 		
+		/*
 		while(!vrstaKrogcev.isEmpty())
 		{
 			krogci.add(vrstaKrogcev.poll());
 		}
+		*/
 		//krogci.addAll(vrstaKrogcev.toArray());
 		
-		for (Krogec k: krogci)
+		for (Krogec k: vrstaKrogcev)
 		{
 			g.setColor(k.barva);
 			g.fillOval(k.x, k.y, 2*k.r, 2*k.r);
@@ -38,19 +43,21 @@ public class KrogciPanel extends JPanel {
 	
 	public void dodajKrogec(Krogec k)
 	{
-		vrstaKrogcev.offer(k);
+		vrstaKrogcev.add(k);
+		//vrstaKrogcev.offer(k);
 		//krogci.add(vrstaKrogcev.poll());
 		repaint();
 	}
 
-	public ArrayList<Krogec> getKrogci() 
+	public CopyOnWriteArrayList<Krogec> getKrogci() 
 	{
-		return krogci;
+		return vrstaKrogcev;
 	}
 	
 	public void pobrisiSliko()
 	{
-		krogci = new ArrayList<Krogec>();
+		
+		//krogci = new ArrayList<Krogec>();
 		System.out.print("slika pobrisana");
 	}
 	
